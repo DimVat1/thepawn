00ef424ae9a473336c091b27f466315818b37438
Dimitris Vatistas <108431974+DimVat1@users.noreply.github.com>
2023-11-27 23:04:00 +0200

    Update chatbot.php

--- chatbot.php
+++ chatbot.php
@@ -1,11 +1,9 @@
-
-
 <?php
-// Set your OpenAI API key
+// Set your OpenAI API key (consider using environment variables)
 $apiKey = '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';
 
-// Get user message from the request
-$userMessage = isset($_POST['userMessage']) ? $_POST['userMessage'] : '';
+// Sanitize user input
+$userMessage = isset($_POST['userMessage']) ? htmlspecialchars($_POST['userMessage']) : '';
 
 // Check if the user message is not empty
 if (!empty($userMessage)) {
@@ -15,10 +13,8 @@ if (!empty($userMessage)) {
     // Set the OpenAI GPT-3 API endpoint
     $endpoint = 'https://api.openai.com/v1/completions';
 
-    // Set the model name
+    // Set the model name and other parameters
     $model = 'text-davinci-002';
-
-    // Set additional parameters
     $temperature = 0.7;
     $maxTokens = 150;
 
@@ -46,14 +42,23 @@ if (!empty($userMessage)) {
     // Execute cURL request
     $response = curl_exec($ch);
 
+    // Check for cURL and API response errors
+    if ($response === false) {
+        echo json_encode(['error' => 'cURL request failed.']);
+    } else {
+        // Decode and return the response as JSON
+        $responseData = json_decode($response, true);
+
+        if ($responseData === null || !isset($responseData['choices'][0]['text'])) {
+            echo json_encode(['error' => 'Invalid or unexpected API response.']);
+        } else {
+            echo json_encode(['assistantReply' => $responseData['choices'][0]['text']]);
+        }
+    }
+
     // Close cURL session
     curl_close($ch);
-
-    // Decode and return the response as JSON
-    $responseData = json_decode($response, true);
-    echo json_encode(['assistantReply' => $responseData['choices'][0]['text']]);
 } else {
     // Return an error message if the user message is empty
     echo json_encode(['error' => 'User message is empty.']);
 }
-
